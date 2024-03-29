d5a969eae4963c73185f0a015e5dba1fb4691d0b
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-03-29 14:43:08 -0400

    Initial Commit

---
+++ WhatTheHex/WhatTheHexApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhatTheHexApp.swift
+//  WhatTheHex
+//
+//  Created by Tyler Lawrence1 on 3/29/24.
+//
+
+import SwiftUI
+
+@main
+struct WhatTheHexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
