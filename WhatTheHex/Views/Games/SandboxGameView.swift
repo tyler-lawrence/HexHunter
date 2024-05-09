5cf7fa6a0d3a2e803085ce2514154b38fb13d53a
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-05-09 15:54:56 -0400

    added sandbox mode

--- WhatTheHex/Views/Games/SandboxGameView.swift
+++ WhatTheHex/Views/Games/SandboxGameView.swift
@@ -10,38 +10,57 @@ import SwiftUI
 struct SandboxGameView: View {
     @State var playerHexcode = Hexcode()
     
-    var gameDetailsView: RotatingView< some View> {
-        let g = Group{
-            ColorSquareView(title: "", hexcode: playerHexcode, showingCode: true)
-            VStack{
-                Text("R: \(playerHexcode.red.toColorScale())")
-                Text("G: \(playerHexcode.green.toColorScale())")
-                Text("B: \(playerHexcode.blue.toColorScale())")
-            }
-            .font(.title2)
-        }
-            .padding()
-        return RotatingView(content: g, originalOrientation: .horizontal)
-    }
-    
-    
     var body: some View {
         GeometryReader{ geo in
             if geo.size.height > geo.size.width {
                 VStack{
-                    gameDetailsView.original
+                    RotatingView(portraitOrientation: .horizontal){
+                        Group{
+                            Spacer()
+                            ColorSquareView(title: "", hexcode: playerHexcode, showingCode: true)
+                               
+                            Spacer()
+                            VStack{
+                                Text("R: \(playerHexcode.red.toColorScale())")
+                                Text("G: \(playerHexcode.green.toColorScale())")
+                                Text("B: \(playerHexcode.blue.toColorScale())")
+                            }
+                            .frame(width: geo.size.width * 0.3)
+                            .font(.title)
+                            Spacer()
+                        }
+                    }.original
                     Divider()
                     RGBSlidersView(hexcode: $playerHexcode)
                 }
+                
                 .padding()
             } else {
                 HStack{
-                    gameDetailsView.flipped
+                    RotatingView(portraitOrientation: .horizontal){
+                        Group{
+                            ColorSquareView(title: "", hexcode: playerHexcode, showingCode: true)
+                            VStack{
+                                Text("R: \(playerHexcode.red.toColorScale())")
+                                Text("G: \(playerHexcode.green.toColorScale())")
+                                Text("B: \(playerHexcode.blue.toColorScale())")
+                            }
+                            .font(.title2)
+                        }
+                    }.rotated
+                        .frame(width: geo.size.width * 0.2)
                     Divider()
                     RGBSlidersView(hexcode: $playerHexcode)
+                        
                 }
                 .padding()
             }
+        }
+        .onAppear{
+            AudioPlayer.shared.startBackgroundLoop(sound: "PracticeMode", type: "mp3")
+        }
+        .onDisappear{
+            AudioPlayer.shared.stopBackgroundSound()
         }
     }
 }
