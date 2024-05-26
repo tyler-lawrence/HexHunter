88885b92554ae8376ffc66e56d9917f3291e7e51
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-05-26 13:26:27 -0400

    updated tests for data controller to use Submission instead of date

---
+++ WhatTheHex/ViewModels/ColorOfTheDaySummaryView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorOfTheDaySummaryView.swift
+//  WhatTheHex
+//
+//  Created by Tyler Lawrence1 on 5/24/24.
+//
+
+import SwiftUI
+
+struct ColorOfTheDaySummaryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorOfTheDaySummaryView()
+}
