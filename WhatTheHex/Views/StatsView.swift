f620f5445f97d1c16aa9dfaba09ee7e629bf5ad8
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-06-10 15:11:08 -0400

    stats view sorting

--- WhatTheHex/Views/StatsView.swift
+++ WhatTheHex/Views/StatsView.swift
@@ -9,10 +9,26 @@ import SwiftUI
 
 struct StatsView: View {
     let dataController: DataController
+    
     var averageScoreLabel: String {
         dataController.averageScore.formatted(.number.precision(.significantDigits(4)))
     }
     
+    @State var submissionSortMethod: SortMethod = .date
+    
+    var sortedSubmissions: [Submission] {
+        switch submissionSortMethod {
+        case .date:
+            dataController.colorOfTheDaySubmissions.sorted{$0.date > $1.date}
+        case .score:
+            dataController.colorOfTheDaySubmissions.sorted{ $0.accuracy > $1.accuracy }
+        }
+    }
+    
+    enum SortMethod: String, CaseIterable {
+        case date = "Date"
+        case score = "Score"
+    }
     
     var body: some View {
         VStack{
@@ -43,11 +59,18 @@ struct StatsView: View {
                 RoundedRectangle(cornerRadius: 25.0)
                     .foregroundStyle(Material.bar)
             )
+            Picker("Sort", selection: $submissionSortMethod){
+                ForEach(SortMethod.allCases, id: \.self){ method in
+                    Text(method.rawValue)
+                }
+            }
+            .pickerStyle(.segmented)
             Divider()
             ScrollView{
-                ForEach(dataController.colorOfTheDaySubmissions, content: SubmissionRowView.init)
+                ForEach(sortedSubmissions, content: SubmissionRowView.init)
             }
         }
+        .padding()
     }
 }
 
