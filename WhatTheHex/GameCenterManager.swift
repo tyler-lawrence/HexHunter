04ac810d280961066a84a785054a7832ab7e2864
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-08-30 14:04:33 -0400

    lint updates

--- WhatTheHex/GameCenterManager.swift
+++ WhatTheHex/GameCenterManager.swift
@@ -9,24 +9,21 @@ import Foundation
 import GameKit
 
 class GameCenterManager {
-    
     static var shared = GameCenterManager()
-    
     var localPlayer = GKLocalPlayer.local
     private(set) var isGameCenterEnabled: Bool = false
-    
-    func authenticateLocalPlayer(){
+    typealias EntryResult = (GKLeaderboard.Entry?, [GKLeaderboard.Entry])
+    func authenticateLocalPlayer() {
         localPlayer.authenticateHandler = { _, error in
             if error != nil {
                 print(error!.localizedDescription)
-            }  else if self.localPlayer.isAuthenticated {
+            } else if self.localPlayer.isAuthenticated {
                 self.isGameCenterEnabled = true
             } else {
                 print("could not authenticate")
             }
         }
     }
-    
     func uploadScore(_ score: Int, for mode: GameMode) async {
         guard GameCenterManager.shared.isGameCenterEnabled else { return }
         guard let leaderboardID = mode.gameCenterLeaderboardID else { return }
@@ -41,15 +38,17 @@ class GameCenterManager {
             print(error.localizedDescription)
         }
     }
-    
     func fetchLeaderboardEntries(for gameMode: GameMode) async -> [GKLeaderboard.Entry] {
         var entries: [GKLeaderboard.Entry] = []
         guard let leaderboardID = gameMode.gameCenterLeaderboardID else { return entries }
-        
         do {
             let leaderboards  = try await GKLeaderboard.loadLeaderboards(IDs: [leaderboardID])
             if let modeLeaderboard = leaderboards.first(where: {$0.baseLeaderboardID == leaderboardID}) {
-                let leaderboardEntries = try await modeLeaderboard.loadEntries(for: .global, timeScope: .allTime, range: NSRange(1...100))
+                let leaderboardEntries = try await modeLeaderboard.loadEntries(
+                    for: .global,
+                    timeScope: .allTime,
+                    range: NSRange(1...100)
+                )
                 entries = leaderboardEntries.1
             }
         } catch {
@@ -57,21 +56,21 @@ class GameCenterManager {
         }
         return entries
     }
-    
     func fetchPlayerLeaderboardEntry(for gameMode: GameMode) async -> GKLeaderboard.Entry? {
-        var entry: GKLeaderboard.Entry? = nil
+        var entry: GKLeaderboard.Entry?
         guard let leaderboardID = gameMode.gameCenterLeaderboardID else { return entry }
-        
         do {
             let leaderboards  = try await GKLeaderboard.loadLeaderboards(IDs: [leaderboardID])
             if let modeLeaderboard = leaderboards.first(where: {$0.baseLeaderboardID == leaderboardID}) {
-                let fetchedEntry: (GKLeaderboard.Entry?, [GKLeaderboard.Entry]) = try await modeLeaderboard.loadEntries(for: [localPlayer], timeScope: .today)
+                let fetchedEntry: EntryResult = try await modeLeaderboard.loadEntries(
+                    for: [localPlayer],
+                    timeScope: .today
+                )
                 entry = fetchedEntry.0
             }
         } catch {
             print(error.localizedDescription)
         }
-        
         return entry
     }
 }
