e56537aa8497ca88762ab02234af452e8cf113ef
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-08-30 14:14:54 -0400

    lint updates

--- WhatTheHex/Views/SettingsView.swift
+++ WhatTheHex/Views/SettingsView.swift
@@ -8,12 +8,10 @@
 import SwiftUI
 
 struct SettingsView: View {
-    
     @AppStorage("GameKitPreference") var gameKitPreference = true
     @AppStorage("darkModePreferred") var darkModePreferred = false
-    
     var body: some View {
-        Form{
+        Form {
             Toggle("Upload scores to the leaderboard", isOn: $gameKitPreference)
             Toggle("Dark mode", isOn: $darkModePreferred)
         }
