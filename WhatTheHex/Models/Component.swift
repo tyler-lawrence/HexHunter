b957b182dcb01bf8c5b66a48a7d559aab4b70481
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-09-02 15:02:29 -0400

    lint updates

--- WhatTheHex/Models/Component.swift
+++ WhatTheHex/Models/Component.swift
@@ -9,18 +9,15 @@ import Foundation
 /// Stores the two digits of a hexcode
 struct Component: Hashable, Codable {
     let hexCategory: HexCategory
-    
     /// first digit in a hexcode
     var digit1: Int
     /// second digit in a hexcode
     var digit2: Int
-    
     init(hexCategory: HexCategory, digit1: Int, digit2: Int) {
         self.hexCategory = hexCategory
         self.digit1 = digit1
         self.digit2 = digit2
     }
-    
     init?(hexCategory: HexCategory, from str: String) {
         guard str.count == 2 else { return nil }
         guard let digit1 = String(str[str.startIndex]).toHexadecimal() else { return nil }
@@ -29,29 +26,23 @@ struct Component: Hashable, Codable {
         self.digit2 = digit2
         self.hexCategory = hexCategory
     }
-    
     var digit1Display: String {
         digit1.toHexadecimalString() ?? ""
     }
-    
     var digit2Display: String {
         digit2.toHexadecimalString() ?? ""
     }
-    
     var display: String {
         "\(digit1Display)\(digit2Display)"
     }
-    
     /// formatted string representing component on 0 - 255 scale
     var colorScaleLabel: String {
         String(self.toColorScale())
     }
-    
     /// formatted string representing component on 0 - 1 scale
     var normalizedLabel: String {
         String(self.colorScaleNormalized())
     }
-    
     /// converts a component its numeric representation
     /// digit 1 * 16 + digit 2
     /// FF == 15, 15
@@ -60,7 +51,6 @@ struct Component: Hashable, Codable {
     func toColorScale() -> Int {
         (digit1 * 16) + digit2
     }
-    
     /// normalizes a color scale to a double between 0 and 1 for use in a color init
     /// digit 1 * 16 + digit 2
     /// FF == 15, 15
