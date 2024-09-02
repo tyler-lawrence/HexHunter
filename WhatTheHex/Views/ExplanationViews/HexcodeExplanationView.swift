b495e1574e6d81045cb0c724d93b8602078ed634
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-09-02 15:13:02 -0400

    fixed spacing with multi line string

--- WhatTheHex/Views/ExplanationViews/HexcodeExplanationView.swift
+++ WhatTheHex/Views/ExplanationViews/HexcodeExplanationView.swift
@@ -43,27 +43,30 @@ struct HexcodeExplanationView: View {
     // MARK: Strings
     var introMessage: String {
     """
-    A hexcode is a hexadecimal representation of a color.\
-    You may have seen a color picking tool request a number from 0 to 255.\
-    Let's explore how we can convert something like #\(hexcode.display) to the 0-255 scale.
+    A hexcode is a hexadecimal representation of a color. \
+    You may have seen a color picking tool request a number from 0 to 255. \
+    Let's explore how we can convert something like #\(hexcode.display) \
+    to the 0-255 scale.
     """
     }
     let introAlphaMessage: String =
     """
     Sometimes you also see alpha attached to a hexcode.\
-    Alpha represents the opacity of a color.
+     Alpha represents the opacity of a color.
     """
     let scaleMessage =
     """
     The scale goes from 00 to FF. \
-    The letters are introduced when we would get to double digit numbers.\
+    The letters are introduced when we would get to double digit numbers. \
     10 becomes A, 11 becomes B, 12 becomes C, etc. \
-    The first number is a large scale change and the second character is finer detail
+    The first number is a large scale change and the second character is \
+    finer detail
     """
     let conversionIntroMessage =
     """
-    To convert the hexcode to a number you first convert the digit\
-    to its corresponding numeric value (if necessary) and apply the following formula:
+    To convert the hexcode to a number you first convert the digit \
+    to its corresponding numeric value (if necessary) and apply the \
+    following formula:
     """
 }
 
