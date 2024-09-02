87cf2a98bbbf0d76371af1d2e87421007fb382f6
Tyler Lawrence <tyler.lawrence15@gmail.com>
2024-09-02 15:03:06 -0400

    lint updates

--- WhatTheHex/Extensions/DataController+Stats.swift
+++ WhatTheHex/Extensions/DataController+Stats.swift
@@ -8,17 +8,15 @@
 import Foundation
 
 extension DataController {
-    //MARK: Player
+    // MARK: Player
     var colorOfTheDayStreak: Int {
-        colorOfTheDaySubmissions.map{$0.date}.streakCount()
+        colorOfTheDaySubmissions.map {$0.date}.streakCount()
     }
-    
-    //MARK: Color of the Day
+    // MARK: Color of the Day
     var totalGamesPlayed: Int {
         colorOfTheDaySubmissions.count
     }
-    
     var averageScore: Double {
-        Double(colorOfTheDaySubmissions.map{$0.accuracy}.reduce(0.0, +) / Double(colorOfTheDaySubmissions.count))
+        Double(colorOfTheDaySubmissions.map {$0.accuracy}.reduce(0.0, +) / Double(colorOfTheDaySubmissions.count))
     }
 }
