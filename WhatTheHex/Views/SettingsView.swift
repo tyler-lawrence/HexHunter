902e01707f177a4200223f476b101f53014e4488
Tyler Lawrence <tyler.lawrence15@gmail.com>
2025-09-28 16:34:11 -0400

    user can now mute music

--- WhatTheHex/Views/SettingsView.swift
+++ WhatTheHex/Views/SettingsView.swift
@@ -8,18 +8,24 @@
 import SwiftUI
 
 struct SettingsView: View {
+    @Bindable(AudioPlayer.shared) var audioPlayer
     @AppStorage(DefaultsKey.gameCenterPreference) var gameKitPreference = true
     @AppStorage(DefaultsKey.preferredAppTheme) private var preferredAppTheme: AppTheme = .system
 
     var body: some View {
         Form {
-            Toggle("Upload scores to the leaderboard", isOn: $gameKitPreference)
+            Section("Game Center") {
+                Toggle("Upload scores to the leaderboard", isOn: $gameKitPreference)
+            }
             Section("Theme") {
                 Picker("App Theme", selection: $preferredAppTheme) {
                     ForEach(AppTheme.allCases) { theme in
                         Text(theme.displayName).tag(theme)
                     }
                 }
+            }
+            Section("Sounds") {
+                Toggle("Music", isOn: $audioPlayer.musicEnabled)
             }
             Section("Notifications") {
                 Button("Open iOS Settings") {
